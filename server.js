ea8ca950401fc32c1da624f0435ba3b19f32fd01
Rakib <bsse1320@iit.du.ac.bd>
2025-01-07 03:34:08 +0600

    ok

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
-const express = require('express');
-const app = express();
+const app = require('./app'); // Import the app from app.js
 const port = 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello, World this is a practice for CICD!');
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
--- test/app.test.js
+++ test/app.test.js
@@ -1,10 +1,10 @@
 const request = require('supertest');
-const app = require('../server'); // server.js ke import kora
+const app = require('../app'); // Import the app
 
-describe("GET /", () => {
+describe('GET /', () => {
   it("should return 'Hello, World this is a practice for CICD!'", async () => {
-    const response = await request(app).get("/");
-    expect(response.statusCode).toBe(200);
+    const response = await request(app).get('/');
     expect(response.text).toBe("Hello, World this is a practice for CICD!");
+    expect(response.status).toBe(200);
   });
 });
